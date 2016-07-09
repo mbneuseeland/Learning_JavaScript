deb3829cc2fa170e04ef44c3cfc8e7879d51aa62
mbneuseeland <michael.brandt@colenet.de>
2016-07-09 15:02:12 +0200

    Start

---
+++ JavaScript/prj_1/script.js
@@ -0,0 +1,28 @@
+
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hallo Jessica, alte Schreppe';
+
+var my_boolean = true;
+var my_first_array = [1,'Jessica',2,'Michael'];
+
+document.write('Wert='   + my_first_array);
+
+document.write("\n\n");
+//alert("Hallo");
+
+document.write('Array = '   + my_boolean);
+
+console.log("Hello World the first");
+
+var iceCream = 'jessica';
+if (iceCream === 'jessica') {
+  alert('Yay, I know jes chocolate ice cream!');
+} else {
+  alert('no, I do not now her');
+}
+
+
+//document.write("Document Write: Hello World!");
+//alert("Hello World!");
+// document.write( 2+8);
